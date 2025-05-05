758a257a622d6dc5dfb17233f7978b06244bbad2
dumplingcompromise <ngoldovsky@gmail.com>
2025-05-05 19:06:30 -0400

    minikit conversion

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -1,14 +1,15 @@
 // src/components/OnchainProviders.tsx
 'use client'
 
-import { OnchainKitProvider } from '@coinbase/onchainkit'
+// import { OnchainKitProvider } from '@coinbase/onchainkit'
 import type { ReactNode } from 'react'
 import { base } from 'viem/chains'
+import { MiniKitProvider } from '@coinbase/onchainkit/minikit'
 
 export default function OnchainProviders({ children }: { children: ReactNode }) {
   
   return (
-    <OnchainKitProvider
+    <MiniKitProvider
       projectId={process.env.NEXT_PUBLIC_CDP_PROJECT_ID!}
       apiKey={process.env.NEXT_PUBLIC_CDP_API_KEY!}
       chain={base}
@@ -34,6 +35,6 @@ export default function OnchainProviders({ children }: { children: ReactNode }) 
       }}
     >
       {children}
-    </OnchainKitProvider>
+    </MiniKitProvider>
   )
 }
